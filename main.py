dd930ef3d84a3c1e6d169e436d130a7963ea8b18
Toni <toninhocabralamorim@gmail.com>
2024-07-19 11:15:50 -0300

    mecanica de andar

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ COMIDA = (255, 0, 0) #vermelho
 pos_x = LAR // 2
 pos_y = ALT // 2
 raio = 25
-
+velocidade = 0.1
 jogando = True
 def circulo(x, y, raio, cor):
     pygame.draw.circle(tela, cor, (int(x), int(y)), int(raio))
@@ -32,10 +32,28 @@ while jogando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jogando = False
+    #atualizar a posicao do  bro
+    keys = pygame.key.get_pressed()
+    velocidade_x = 0
+    velocidade_y = 0
+    if(keys[pygame.K_RIGHT]):
+        velocidade_x += 1
+    if(keys[pygame.K_LEFT]):
+        velocidade_x -=  1
+    if(keys[pygame.K_UP]):
+        velocidade_y -= 1
+    if(keys[pygame.K_DOWN]):
+        velocidade_y += 1
     
+    velocidade_x *= velocidade
+    velocidade_y *= velocidade
+    pos_x += velocidade_x
+    pos_y += velocidade_y
+
     #desenhar
     tela.fill(FUNDO)
     circulo(pos_x, pos_y, raio, PLAYER)
+    circulo(0,0, 100, COMIDA) #circulo aleatorio so p saber se ele ta se mexendo
     pygame.display.flip()
 
 print("saindo")
@@ -44,7 +62,8 @@ Todo:
 
 [X] - Abrir o pygame 
 [X] - Mostrar o player 
-[ ] - Fazer o player andar 
+[X] - Fazer o player andar
+[ ] - Fazer a camera seguir o mano player 
 [ ] - Spawnar comida ao redor do player
 [ ] - Detectar colisão entre player e comida
 [ ] - Animação da comida caindo no buraco
